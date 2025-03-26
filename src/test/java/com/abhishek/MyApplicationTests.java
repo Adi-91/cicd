f4b4dfce56134f3eb587c07750fb50b8c5601716
adi <jagtapadi079@gmail.com>
2025-03-26 15:40:08 +0000

    added

--- src/test/java/com/abhishek/MyApplicationTests.java
+++ src/test/java/com/abhishek/MyApplicationTests.java
@@ -13,7 +13,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class MyApplicationTests {
+class MyApplicationTests {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
